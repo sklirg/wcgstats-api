386df3a89d34880d00d7abab34fcf6af2b415dba
sklirg <hlsolbjorg@gmail.com>
2016-11-25 01:26:45 +0100

    Get static root from env

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
-import "gopkg.in/gin-gonic/gin.v1"
+import (
+	"os"
+
+	"gopkg.in/gin-gonic/gin.v1"
+)
 
 func main() {
 	// Creates a gin router with default middleware:
 	// logger and recovery (crash-free) middleware
 	router := gin.Default()
 
-	router.Static("/client", "./public")
-	router.Static("/static", "./public/static")
+	static_path := os.Getenv("WCGSTATSAPI_STATIC_ROOT")
+	if static_path == "" {
+		static_path = "./public"
+	}
+
+	router.Static("/client", static_path)
+	router.Static("/static", static_path+"/static")
 
 	v1 := router.Group("api/v1")
 	team_routes := v1.Group("teams")
